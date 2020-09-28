7ecb1ba80f6590635de22031a0f847de6c170843
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-09-27 23:43:02 -0500

    add rocket speed setting

--- TeardownCameraHack/TeardownSettings.cs
+++ TeardownCameraHack/TeardownSettings.cs
@@ -18,6 +18,12 @@ namespace TeardownCameraHack
             set => Writer.Default.Write(_teardownBaseAddress + 0x2EE638, value);
         }
 
+        public float RocketSpeed
+        {
+            get => Reader.Default.Read<float>(_teardownBaseAddress + 0x2EDF98, out _);
+            set => Writer.Default.Write(_teardownBaseAddress + 0x2EDF98, value);
+        }
+
         public float BulletDespawnDistance
         {
             get => Reader.Default.Read<float>(_teardownBaseAddress + 0x2F2BAC, out _);
