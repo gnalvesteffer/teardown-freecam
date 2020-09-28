2724038d4348ffd877fa01f8c8a2cd15c5699114
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-09-28 02:32:57 -0500

    rename mouse position properties

--- TeardownCameraHack/TeardownModels/TeardownInput.cs
+++ TeardownCameraHack/TeardownModels/TeardownInput.cs
@@ -6,13 +6,13 @@ namespace TeardownCameraHack.TeardownModels
     {
         private readonly ulong _address;
 
-        public int MouseScreenPositionX
+        public int MouseWindowPositionX
         {
             get => Reader.Default.Read<int>(_address + 0x0, out _);
             set => Writer.Default.Write(_address + 0x0, value);
         }
 
-        public int MouseScreenPositionY
+        public int MouseWindowPositionY
         {
             get => Reader.Default.Read<int>(_address + 0x4, out _);
             set => Writer.Default.Write(_address + 0x4, value);
